6ebe375f721c36b5880f4d2486adb3f726947088
Matheus Chaves <matheusca@hotmail.com>
2023-07-17 14:32:20 -0300

    Section 2 created, featured btns and links not functioning yet

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -14,7 +14,7 @@ function Navbar () {
 
     return(
         <nav className='h-20 flex align-center justify-between shadow-md'>
-            <img className='object-contain bg-blend-color-burn w-40 h-35 ml-10' src={logo} alt='unbk logo' />
+            <img className='object-contain w-40 h-35 ml-10' src={logo} alt='unbk logo' />
             <div className='w-96 flex justify-around items-center mr-10 font-extrabold'> 
                 <div>
                     <ListItemButton onClick={handleClick}>
@@ -22,7 +22,7 @@ function Navbar () {
                         { open ? <ExpandLess /> : <ExpandMore />}
                     </ListItemButton>
                     <Collapse in={open} className='fixed' timeout="auto" unmountOnExit>
-                        <List className='bg-slate-100 w-40 h-40' component="div" disablePadding>
+                        <List className='bg-black text-white w-40 h-40' component="div">
                             <ListItemText className='pl-1' /> UNBK PRO
                             <ListItemText /> UNBK LIFT
                             <ListItemText /> UNBK SWEAT
